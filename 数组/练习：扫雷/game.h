71e8dba3b19a7fd5031eb8e40e73856f0af35fc8
J0kerCoder <1005103630@qq.com>
2026-08-31 21:16:21 +0800

    addbomb

--- 数组/练习：扫雷/game.h
+++ 数组/练习：扫雷/game.h
@@ -4,7 +4,15 @@
 #define COLS COL + 2
 
 
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
+
+void newbroad( char arr[ROWS][COLS], int row , int col , char x);
+
+void printbroad( char arr[ROWS][COLS], int row , int col);
+
+void addbomb(char arr[ROWS][COLS], int row, int col);
+
+//void printbroad( char arr[ROWS][COLS], int row , int col);
